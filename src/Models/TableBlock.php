162f404be8b0da9cfd6f20c38891d56ff3615927
Lani Field <lani.field@signify.co.nz>
2021-02-18 18:57:12 +1300

    feat: Modify edit form for table.

--- src/Models/TableBlock.php
+++ src/Models/TableBlock.php
@@ -7,8 +7,10 @@ use Signify\Factory\Models\TableItem;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\GridField\GridFieldButtonRow;
 use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\TextareaField;
 use SilverStripe\ORM\FieldType\DBField;
 use Symbiote\GridFieldExtensions\GridFieldAddNewInlineButton;
 use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
@@ -60,6 +62,85 @@ class TableBlock extends BaseElement
         8 => 'Eight',
     ];
 
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $tableDescription = HTMLEditorField::create(
+            'TableDescription'
+        );
+        $tableDescription->setRows(4);
+        $fields->replaceField('TableDescription', $tableDescription);
+
+        $tableCaption = TextareaField::create(
+            'TableCaption'
+        );
+        $tableCaption->setRows(2);
+        $fields->replaceField('TableCaption', $tableCaption);
+
+        // Only modify relation fields if DO present in DB
+        if ($this->ID) {
+            $gridField = $fields->fieldByName('Root.TableItems.TableItems');
+
+            // Make a custom config supporting inline editing
+            $config = GridFieldConfig::create()
+                ->addComponent(new GridFieldButtonRow('before'))
+                ->addComponent(new GridFieldToolbarHeader())
+                ->addComponent(new GridFieldTitleHeader())
+                ->addComponent($edCols = new GridFieldEditableColumns())
+                ->addComponent(new GridFieldDeleteAction())
+                ->addComponent(new GridFieldAddNewInlineButton())
+                ->addComponent(new GridFieldSortableRows('SortOrder'));
+            $gridField->setConfig($config);
+
+            // Modify {@see TextAreaField} instances to have 2 rows by default
+            $cols = $this->NumberOfColumns;
+            $disp = [];
+            foreach (range(1, $cols) as $i) {
+                $prop = 'Cell' . $i;
+                $disp[$prop] = function ($record, $column, $grid) {
+                    return HTMLEditorField::create($column)->setRows(2);
+                };
+            }
+
+            $edCols->setDisplayFields($disp);
+        };
+
+        // Use dropdown field so we can limit he range of numbers
+        $numberOfColumns = DropdownField::create(
+            'NumberOfColumns',
+            'Number of Columns',
+            array_combine(
+                range(1, 8),
+                range(1, 8)
+            )
+        );
+        $fields->replaceField('NumberOfColumns', $numberOfColumns);
+
+        $getFields = function (&$fields, $fieldNames, $tabName) {
+            foreach ($fieldNames as $fieldName) {
+                $field = $fields->dataFieldByName($fieldName);
+
+                if ($field) {
+                    $fields->addFieldToTab($tabName, $field);
+                }
+            }
+        };
+
+        // Move column setting fields to columns
+        $getFields(
+            $fields,
+            [
+                'NumberOfColumns',
+                'FirstRowIsHeader',
+                'LastRowIsFooter'
+            ],
+            'Root.Settings'
+        );
+
+        return $fields;
+    }
+
     /**
      * Returns a preview of the selected settings for display in the CMS.
      * @return string
@@ -98,5 +179,31 @@ class TableBlock extends BaseElement
     public function inlineEditable()
     {
         return false;
+    }
+
+    /**
+     * Given $prop convert any integers to the corresponding word.
+     *
+     * @param string $prop
+     * @return string
+     */
+    public function beautifyIntProp($prop)
+    {
+        $regex = '/([^\d]*)(\d)(.*)/m';
+
+        preg_match($regex, $prop, $matches);
+
+        $returnString = $matches[0];
+
+        if (count($matches) > 1) {
+            $returnString = sprintf(
+                '%s %s %s',
+                $matches[1],
+                static::$intToWordMap[$matches[2]],
+                $matches[3]
+            );
+        }
+
+        return $returnString;
     }
 }
