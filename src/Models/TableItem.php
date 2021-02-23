55d1173852d1edba3baed1e275c5885fdddeabe1
Lani Field <lani.field@signify.co.nz>
2021-02-23 17:37:56 +1300

    feat: Add getCMSFields function to remove unwanted cell fields.
    Also has code to replace used cell fields with custome HTMLEditor config (but not sure if this is really necessary as done on block level).

--- src/Models/TableItem.php
+++ src/Models/TableItem.php
@@ -3,6 +3,7 @@
 namespace Signify\Factory\Models;
 
 use Signify\Factory\Models\TableBlock;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\ORM\DataObject;
 
 class TableItem extends DataObject
@@ -32,4 +33,29 @@ class TableItem extends DataObject
     ];
 
     private static $default_sort = 'SortOrder';
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $block = TableBlock::get_by_id($this->TableBlockID);
+        $cols = $block->NumberOfColumns;
+        foreach (range(1, $cols) as $i) {
+            $column = 'Cell' . $i;
+            $colField = HTMLEditorField::create($column)
+                ->setEditorConfig('cellTinyMCE')
+                ->setValue('<p>&nbsp;</p>')
+                ->setRows(4);
+            $fields->replaceField($column, $colField);
+        }
+        foreach (range($cols + 1, 8) as $i) {
+            $column = 'Cell' . $i;
+            $fields->removeByName($column);
+        }
+
+        $fields->removeByName('SortOrder');
+        $fields->removeByName('TableBlockID');
+
+        return $fields;
+    }
 }
